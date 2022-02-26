934a6fe59bfa0d19d2da6fcc318a37ac477c1316
Fudgey <fudgebucket27@gmail.com>
2022-02-26 11:11:15 +1000

    Add project files.

---
+++ LoopPin/Models/PinataData.cs
@@ -0,0 +1,10 @@
+﻿namespace LoopPin.Models
+{
+    public class PinataData
+    {
+
+        public string IpfsHash { get; set; }
+        public int PinSize { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ LoopPin/Services/PinataService.cs
@@ -0,0 +1,29 @@
+﻿using LoopPin.Models;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace LoopPin.Services
+{
+    public class PinataService
+    {
+        const string _baseUrl = "https://api.pinata.cloud";
+
+        readonly RestClient _client;
+
+        public PinataService()
+        {
+            _client = new RestClient(_baseUrl);
+        }
+
+        public async Task<PinataData> SubmitNFTImage(byte[] fileBytes, string fileName)
+        {
+            var request = new RestRequest("pinning/pinFileToIPFS");
+            request.AddHeader("pinata_api_key", Environment.GetEnvironmentVariable("APPSETTING_APIKEY"));
+            request.AddHeader("pinata_secret_api_key", Environment.GetEnvironmentVariable("APPSETTING_APISECRET"));
+            request.AddFile("file", fileBytes, fileName);
+            var response = await _client.PostAsync(request);
+            var data = JsonConvert.DeserializeObject<PinataData>(response.Content!);
+            return data;
+        }
+    }
+}
