a80950e8cf12ccbf7cf5f44933fc8e4bf8045f19
Fudgey <fudgebucket27@gmail.com>
2022-03-01 04:43:17 +1000

    fix for machine target

--- LoopPin/Program.cs
+++ LoopPin/Program.cs
@@ -20,8 +20,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    ApiKeyHelper._apiKey = Environment.GetEnvironmentVariable("APPSETTING_PINATAAPIKEY", EnvironmentVariableTarget.Machine);
-    ApiKeyHelper._apiKeySecret = Environment.GetEnvironmentVariable("APPSETTING_PINATAAPIKEYSECRET", EnvironmentVariableTarget.Machine);
+    ApiKeyHelper._apiKey = Environment.GetEnvironmentVariable("APPSETTING_PINATAAPIKEY");
+    ApiKeyHelper._apiKeySecret = Environment.GetEnvironmentVariable("APPSETTING_PINATAAPIKEYSECRET");
 }
 else
 {
