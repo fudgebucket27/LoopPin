94bdcedce6591378d1aeefb14864cc2869deffea
Fudgey <fudgebucket27@gmail.com>
2022-03-16 08:10:36 +1000

    new metadata

--- LoopPin/Models/NftMetadata.cs
+++ LoopPin/Models/NftMetadata.cs
@@ -5,5 +5,7 @@
         public string name { get; set; }
         public string description { get; set; }
         public string image { get; set; }
+
+        public int royalty_percentage { get; set; }
     }
 }
